ff49307dd518347cec5812395901b909e5b5de3a
AlekVolsk <AlekVolsk@users.noreply.github.com>
2024-10-24 13:35:25 +0400

    Update servers.php

--- servers.php
+++ servers.php
@@ -4,22 +4,22 @@
 
 $servers = [
     [
-        'name' => 'Joomla! Core Stable (3.x & 4.x)',
+        'name' => 'Joomla! Core Stable (3.x/4.x/5.x)',
         'zip' => true,
         'url' => 'https://update.joomla.org/core/sts/list_sts.xml'
     ],
     [
-        'name' => 'Joomla! Core Test (3.x & 4.x)',
+        'name' => 'Joomla! Core Test (3.x/4.x/5.x)',
         'zip' => true,
         'url' => 'https://update.joomla.org/core/test/list_test.xml'
     ],
     [
-        'name' => 'Joomla! Update component (3.x & 4.x)',
+        'name' => 'Joomla! Update component (3.x/4.x/5.x)',
         'zip' => true,
         'url' => 'https://update.joomla.org/core/extensions/com_joomlaupdate.xml'
     ],
     [
-        'name' => 'Joomla! Installer - Install from Web (3.x & 4.x)',
+        'name' => 'Joomla! Installer - Install from Web (3.x/4.x/5.x)',
         'zip' => true,
         'url' => 'https://appscdn.joomla.org/webapps/jedapps/webinstaller.xml'
     ],
@@ -32,5 +32,10 @@ $servers = [
         'name' => 'Joomla! 4.x translations',
         'zip' => false,
         'url' => 'https://update.joomla.org/language/translationlist_4.xml'
+    ],
+    [
+        'name' => 'Joomla! 5.x translations',
+        'zip' => false,
+        'url' => 'https://update.joomla.org/language/translationlist_5.xml'
     ]
 ];
